62322d06998a868b6f23080ba101a71d70654127
Fabian Schlenz <mail@fabianonline.de>
2012-04-23 14:03:26 +0200

    Added SinatraWMS::get_html_for_map_at() to quickly generate HTML code.

--- lib/sinatra_wms.rb
+++ lib/sinatra_wms.rb
@@ -12,4 +12,37 @@ module SinatraWMS
 	def self.deg_sin(x)
 		Math.sin(x * Math::PI / 180)
 	end
+	
+	def self.get_html_for_map_at(url, options={})
+		options[:title] ||= "Sinatra-WMS"
+		options[:opacity] ||= 1
+		options[:datasource_name] ||= "WMS-Data"
+
+		%Q{
+		<html>
+			<head>
+				<title>#{options[:title]}</title>
+				<script src="http://openlayers.org/api/OpenLayers.js"></script>
+				<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+			</head>
+			<body>
+				<div style="width:100%; height:100%" id="map"></div>
+				<script type="text/javascript" defer="defer">
+					var imagebounds = new OpenLayers.Bounds(-20037508.34, -20037508.34, 20037508.34, 20037508.34);
+
+					var map = new OpenLayers.Map('map');
+					map.addControl(new OpenLayers.Control.LayerSwitcher());
+
+					var osm_layer = new OpenLayers.Layer.OSM();
+					osm_layer.setOpacity(#{options[:opacity]});
+					map.addLayer(osm_layer);
+
+					var layer = new OpenLayers.Layer.WMS("#{options[:datasource_name]}", "#{url}", {transparent: true}, {maxExtent: imagebounds});
+
+					map.addLayer(layer);
+					map.zoomToExtent(imagebounds);
+				</script>
+			</body>
+		</html>}
+	end
 end
